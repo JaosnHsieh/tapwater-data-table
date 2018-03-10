6d319d6f92c5f317c8f8c9c127fae35dbd63f956
JaosnHsieh <appleoxxo@gmail.com>
2018-03-10 02:45:08 +0000

    done select limit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import "./App.css";
 import axios from "axios";
 const corsServer = `http://45.32.119.183:8080/`;
 const tapwaterDataUrl = `http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=6805942d-a8dd-4635-a394-01a8eadb3eba`;
-const instance = axios.create({
-  baseURL: `${corsServer}${tapwaterDataUrl}`,
-  timeout: 10000
-});
+
 // http://data.taipei/opendata/datalist/apiAccess?scope=datasetMetadataSearch
 // &q=臺北市文化快遞資訊&sort=metadata_created desc
 
@@ -21,21 +18,65 @@ class App extends Component {
       limit: 10,
       offset: 0,
       data: [],
+      count: 0,
       isLoading: false
     };
   }
-  async componentDidMount() {
-    let { data: { result: { results: data } } } = await instance.get("");
-
-    this.setState({ data });
+  componentDidMount() {
+    console.log("didmout");
+    this.getData();
   }
-  getData = async () => {};
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return (
+  //     nextState.limit !== this.state.limit ||
+  //     nextState.offset !== this.state.offset
+  //   );
+  // // }
+  componentDidUpdate(previousProps, previousState) {
+    if (
+      previousState.limit !== this.state.limit ||
+      previousState.offset !== this.state.offset
+    ) {
+      this.getData();
+    }
+  }
+  getData = async (limit = 10, offset = 0) => {
+    const baseURL = `${corsServer}${tapwaterDataUrl}`;
+    const query = `${baseURL}&limit=${this.state.limit}`;
+    let { data: { result } } = await axios.get(query);
+    console.log("result", result);
+    const { count, results: data } = result;
+
+    this.setState({ data, count });
+  };
+  onLimitChange = async e => {
+    this.setState({
+      limit: e.target.value
+    });
+  };
   render() {
+    console.log("render");
+
     const { data } = this.state;
-    console.log(data);
     return (
       <div className="App">
         <header className="App-header">臺北市自來水管承裝商業者</header>
+        <nav className="controller">
+          <span>資料總筆數:{this.state.count}</span>
+          <span>
+            筆數
+            <select onChange={this.onLimitChange} value={this.state.limit}>
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="50">50</option>
+              <option value="100">100</option>
+            </select>
+          </span>
+          <span>
+            {" "}
+            {"<<"} 上一頁 下一頁{">>"}
+          </span>
+        </nav>
         <section className="table">
           {data.length > 0 && (
             <div>
@@ -46,7 +87,15 @@ class App extends Component {
               <span>OWNER</span>
             </div>
           )}
-          {/* {data.map(() => 1)} */}
+          {data.map(ele => (
+            <div key={ele._id}>
+              <span>{ele._id}</span>
+              <span>{ele.NAME}</span>
+              <span>{ele.COMNUM}</span>
+              <span>{ele.ADDR}</span>
+              <span>{ele.OWNER}</span>
+            </div>
+          ))}
         </section>
       </div>
     );
